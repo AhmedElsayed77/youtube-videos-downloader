7908103f8bcc34bea5a6f1883bab49249db9cf80
Ahemd Eldeep <84293988+eldepp@users.noreply.github.com>
2022-12-03 18:23:58 +0200

    Update youtube videos downloader.py

--- youtube videos downloader.py
+++ youtube videos downloader.py
@@ -1,17 +1,21 @@
-# For Downloading a Viedo:
+#Notice : You must downlaod Pytube library by commands in cmd : pip install git+https://github.com/pytube/pytube
+
+# For Downloading a Video:
 
 from pytube import YouTube
-link = "the viedo link" # put your video link here
+link_url = input("Enter the video url")
+link = link_url # put your video link here
 video =YouTube(link)
 
 def finish():
     print ("Download Done") #Confirmation that the video downloaded
 
  #for hight resolution and output_path for the folder you need the video to be in 
-video.streams.get_highest_resolution().download(output_path="C:/Users/Kimo Store/Documents/learn")
+output_path_url = input("Enter the video path") #C:/Users/Kimo Store/Documents/learn
+video.streams.get_highest_resolution().download(output_path= output_path_url)
 
 video.register_on_complete_callback(finish()) #calling the confrimation function
-
+ 
 
 # For Downloading a playlist:
 
